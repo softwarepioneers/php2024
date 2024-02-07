1415e75dabadffcf58f0f7c27a80640653d4fc9c
softwarepioneers <hiigsitechnology@outlook.com>
2024-02-07 13:51:30 +0300

    sdssdasasa

--- PDO1.php
+++ PDO1.php
@@ -11,11 +11,11 @@ $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,
 array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
 echo "Connected successfully";
-$dbh = null;
+// $dbh = null;
 catch (PDOException $e)
 {
 exit("Error: " . $e->getMessage());
 echo "Connected failed";
-$dbh = null;
+// $dbh = null;
 }
 ?>
